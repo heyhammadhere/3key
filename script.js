b4d496742c73fcb66852b553acd806a2be124a2c
HeyHammadHere <heyhammadhere@gmail.com>
2021-08-24 21:02:33 +0500

    v0.1.0

--- script.js
+++
@@ -1,7 +0,0 @@
-(() => {
-  let hamburger = document.querySelector("div.hamburger");
-  let navLinks = document.querySelector("ul.nav-links");
-  hamburger.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
-  });
-})();
