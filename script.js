fc6e05fb976592859548d705671b02c9fc6ff400
HeyHammadHere <heyhammadhere@gmail.com>
2021-08-30 05:09:19 +0500

    v0.1.0

---
+++ script.js
@@ -0,0 +1,34 @@
+$(function () {
+  $("path.cls-31, path.cls-35").hover(
+    () => {
+      $("path.cls-35").show();
+    },
+    () => {
+      $("path.cls-35").hide();
+    }
+  );
+  $("path.cls-35").hover(
+    () => {
+      $("path.cls-35").show();
+    },
+    () => {
+      $("path.cls-35").hide();
+    }
+  );
+  $("path.cls-30, path.cls-34").hover(
+    () => {
+      $("path.cls-34").show();
+    },
+    () => {
+      $("path.cls-34").hide();
+    }
+  );
+  $("path.cls-32, path.cls-42").hover(
+    () => {
+      $("path.cls-42").show();
+    },
+    () => {
+      $("path.cls-42").hide();
+    }
+  );
+});
